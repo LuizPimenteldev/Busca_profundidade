774b17db852e804d85cdf6791ad4564766840fcb
Luiz Eduardo <124412405+LuizPimenteldev@users.noreply.github.com>
2026-03-16 18:13:47 -0300

    buscas_gulosa_heuristica
    Added new locations and updated heuristics for pathfinding.

--- buscas.py
+++ buscas.py
@@ -1,94 +1,78 @@
-
 grafo = {
-    'A': {'C': 14, 'B': 12},
-    'B': {'A': 12, 'C': 9, 'D': 38},
-    'C': {'A': 14, 'B': 9, 'D': 24, 'E': 7},
-    'D': {'B': 38, 'C': 24, 'E': 13, 'G': 9},
-    'E': {'C': 7, 'D': 13, 'F': 9, 'G': 29},
-    'F': {'E': 9},
-    'G': {'D': 9, 'E': 29}
+    'Maceio':    {'Recife': 285, 'Arapiraca': 128},
+    'Arapiraca': {'Maceio': 128, 'Recife': 270, 'Caruaru': 210},
+    'Recife':    {'Maceio': 285, 'Arapiraca': 270, 'Caruaru': 130, 'Natal': 295},
+    'Caruaru':   {'Arapiraca': 210, 'Recife': 130, 'Natal': 390, 'Campina': 185},
+    'Natal':     {'Recife': 295, 'Caruaru': 390, 'Joao_Pessoa': 185, 'Campina': 170},
+    'Joao_Pessoa':{'Natal': 185},
+    'Campina':   {'Caruaru': 185, 'Natal': 170}
 }
 
-# Heurística
+# Heurística: distância estimada até Campina Grande
 heuristica = {
-    'A': 30,
-    'B': 26,
-    'C': 21,
-    'D': 7,
-    'E': 22,
-    'F': 36,
-    'G': 0
+    'Maceio':     370,
+    'Arapiraca':  310,
+    'Recife':     130,
+    'Caruaru':     70,
+    'Natal':      170,
+    'Joao_Pessoa':360,
+    'Campina':      0
 }
-
 
 def busca_gulosa(inicio, objetivo):
-
-    abertos = [inicio] #Lista de nós que ainda podem ser explorados.
-    caminho = [] # Lista para guardar o caminho percorrido.
-    visitados = set() # quais já foram visitados
+    abertos = [inicio]
+    caminho = []
+    visitados = set()
 
     while abertos:
-
         atual = min(abertos, key=lambda no: heuristica[no])
-        abertos.remove(atual) # O nó escolhido sai da lista de candidatos.
-
-        caminho.append(atual) # e vai para o de caminho percorrido
+        abertos.remove(atual)
+        caminho.append(atual)
         visitados.add(atual)
 
         if atual == objetivo:
             return caminho
 
         for vizinho in grafo[atual]:
-            if vizinho not in visitados and vizinho not in abertos: # Evita repetir nós.
+            if vizinho not in visitados and vizinho not in abertos:
                 abertos.append(vizinho)
 
     return None
 
-
 def busca_a_estrela(inicio, objetivo):
-
     abertos = [inicio]
-    veio_de = {} #mapa de caminhos
+    veio_de = {}
 
     g = {no: float('inf') for no in grafo}
     g[inicio] = 0
 
     f = {no: float('inf') for no in grafo}
-    f[inicio] = heuristica[inicio] 
+    f[inicio] = heuristica[inicio]
 
     while abertos:
+        atual = min(abertos, key=lambda no: f[no])
 
-        atual = min(abertos, key=lambda no: f[no]) #Escolhe o nó com menor f(n).
-
-        if atual == objetivo: 
-
+        if atual == objetivo:
             caminho = []
-            while atual in veio_de: # quando encontra o objetivo ele vai voltando
+            while atual in veio_de:
                 caminho.append(atual)
                 atual = veio_de[atual]
-
             caminho.append(inicio)
-            caminho.reverse() # depois inverte
-
+            caminho.reverse()
             return caminho
 
         abertos.remove(atual)
 
-        for vizinho, custo in grafo[atual].items(): # aqui pega o vizinho e ver o custo
-            #custo até agora + custo da nova aresta
-            g_temp = g[atual] + custo #calcula o g temporario
-
-            if g_temp < g[vizinho]: # faz a verificação se encontrou o menor caminho que custa menos
-
+        for vizinho, custo in grafo[atual].items():
+            g_temp = g[atual] + custo
+            if g_temp < g[vizinho]:
                 veio_de[vizinho] = atual
                 g[vizinho] = g_temp
                 f[vizinho] = g[vizinho] + heuristica[vizinho]
-
                 if vizinho not in abertos:
                     abertos.append(vizinho)
 
     return None
 
-
-print("Caminho busca gulosa:", busca_gulosa('A', 'G'))
-print("Caminho A*:", busca_a_estrela('A', 'G'))
+print("Caminho busca gulosa:", busca_gulosa('Maceio', 'Campina'))
+print("Caminho A*:",           busca_a_estrela('Maceio', 'Campina'))
